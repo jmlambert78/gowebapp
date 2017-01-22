23843bb0f2988059d0e233778e89b416f610cef3
Eric Muellenbach <emuellenbach@gmail.com>
2017-01-22 21:23:44 +0100

    First step for a random password generator

--- main.go
+++ main.go
@@ -3,25 +3,56 @@ package main
 import (
     "fmt"
     "net/http"
-    "strings"
     "log"
+    "math/rand"
+    "time"
+    "strconv"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()  // parse arguments, you have to call this by yourself
-    fmt.Println(r.Form)  // print form information in server side
-    fmt.Println("path", r.URL.Path)
-    fmt.Println("scheme", r.URL.Scheme)
-    fmt.Println(r.Form["url_long"])
-    for k, v := range r.Form {
-        fmt.Println("key:", k)
-        fmt.Println("val:", strings.Join(v, ""))
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const (
+    letterIdxBits = 6                    // 6 bits to represent a letter index
+    letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+    letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+const defaultPasswordLength = 24
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+func RandStringBytesMaskImprSrc(n int) string {
+    b := make([]byte, n)
+    // A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+    for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+        if remain == 0 {
+            cache, remain = src.Int63(), letterIdxMax
+        }
+        if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+            b[i] = letterBytes[idx]
+            i--
+        }
+        cache >>= letterIdxBits
+        remain--
     }
-    fmt.Fprintf(w, "Hello astaxie!") // send data to client side
+
+    return string(b)
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()  // parse arguments, you have to call this by yourself
+    if val, ok := r.Form["l"]; ok {
+        if s, err := strconv.Atoi(val[0]); err == nil {
+            fmt.Fprintf(w, RandStringBytesMaskImprSrc(s))
+        } else {
+            fmt.Fprintf(w, RandStringBytesMaskImprSrc(defaultPasswordLength))
+        }
+    } else {
+        fmt.Fprintf(w, RandStringBytesMaskImprSrc(defaultPasswordLength))
+    }
 }
 
 func main() {
-    http.HandleFunc("/", sayhelloName) // set router
+    http.HandleFunc("/", handleRequest) // set router
     err := http.ListenAndServe(":9090", nil) // set listen port
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
