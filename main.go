faf171918d025940ad898b4a2d229934ae749c5b
Eric Muellenbach <emuellenbach@gmail.com>
2017-01-22 22:14:26 +0100

    Use Docker Service for rolling upgrade

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "log"
-    "math/rand"
-    "time"
-    "strconv"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
 )
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 const (
-    letterIdxBits = 6                    // 6 bits to represent a letter index
-    letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-    letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
 )
 
 const defaultPasswordLength = 24
@@ -21,40 +21,41 @@ const defaultPasswordLength = 24
 var src = rand.NewSource(time.Now().UnixNano())
 
 func RandStringBytesMaskImprSrc(n int) string {
-    b := make([]byte, n)
-    // A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-    for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
-        if remain == 0 {
-            cache, remain = src.Int63(), letterIdxMax
-        }
-        if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-            b[i] = letterBytes[idx]
-            i--
-        }
-        cache >>= letterIdxBits
-        remain--
-    }
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
 
-    return string(b)
+	return string(b)
 }
 
 func handleRequest(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()  // parse arguments, you have to call this by yourself
-    if val, ok := r.Form["l"]; ok {
-        if s, err := strconv.Atoi(val[0]); err == nil {
-            fmt.Fprintf(w, RandStringBytesMaskImprSrc(s))
-        } else {
-            fmt.Fprintf(w, RandStringBytesMaskImprSrc(defaultPasswordLength))
-        }
-    } else {
-        fmt.Fprintf(w, RandStringBytesMaskImprSrc(defaultPasswordLength))
-    }
+	r.ParseForm() // parse arguments, you have to call this by yourself
+	fmt.Fprintf(w, "Output: ")
+	if val, ok := r.Form["l"]; ok {
+		if s, err := strconv.Atoi(val[0]); err == nil {
+			fmt.Fprintf(w, RandStringBytesMaskImprSrc(s))
+		} else {
+			fmt.Fprintf(w, RandStringBytesMaskImprSrc(defaultPasswordLength))
+		}
+	} else {
+		fmt.Fprintf(w, RandStringBytesMaskImprSrc(defaultPasswordLength))
+	}
 }
 
 func main() {
-    http.HandleFunc("/", handleRequest) // set router
-    err := http.ListenAndServe(":9090", nil) // set listen port
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	http.HandleFunc("/", handleRequest)      // set router
+	err := http.ListenAndServe(":9090", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
